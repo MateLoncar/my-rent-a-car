456989b9b3a60ef8db8402d4c2faca8acf875853
Mate Loncar <mloncar@abasoft-it.com>
2025-03-09 11:09:54 +0100

    home page added

--- migrations/Version20250309094907.php
+++ migrations/Version20250309094907.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250308113452 extends AbstractMigration
+final class Version20250309094907 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,9 @@ final class Version20250308113452 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE person (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, date_of_birth DATE DEFAULT NULL, drivers_licence TINYINT(1) NOT NULL, active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE rent (id INT AUTO_INCREMENT NOT NULL, vehicle_id_id INT NOT NULL, person_id_id INT NOT NULL, rent_time DATETIME NOT NULL, return_time DATETIME NOT NULL, start_mileage INT NOT NULL, end_mileage INT NOT NULL, notice LONGTEXT DEFAULT NULL, INDEX IDX_2784DCC1DEB1EBB (vehicle_id_id), INDEX IDX_2784DCCD3728193 (person_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE vehicle (id INT AUTO_INCREMENT NOT NULL, brand VARCHAR(20) NOT NULL, model VARCHAR(20) NOT NULL, type SMALLINT NOT NULL, year_of_production SMALLINT NOT NULL, mileage INT NOT NULL, no_of_seats SMALLINT NOT NULL, no_of_door SMALLINT NOT NULL, active TINYINT(1) NOT NULL, rented TINYINT(1) NOT NULL, notice LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE rent ADD CONSTRAINT FK_2784DCC1DEB1EBB FOREIGN KEY (vehicle_id_id) REFERENCES vehicle (id)');
         $this->addSql('ALTER TABLE rent ADD CONSTRAINT FK_2784DCCD3728193 FOREIGN KEY (person_id_id) REFERENCES person (id)');
@@ -31,7 +33,9 @@ final class Version20250308113452 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE rent DROP FOREIGN KEY FK_2784DCC1DEB1EBB');
         $this->addSql('ALTER TABLE rent DROP FOREIGN KEY FK_2784DCCD3728193');
+        $this->addSql('DROP TABLE person');
         $this->addSql('DROP TABLE rent');
+        $this->addSql('DROP TABLE vehicle');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
